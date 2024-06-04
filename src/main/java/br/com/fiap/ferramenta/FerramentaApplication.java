40b503383851d77294a4cd54782f52d10fb9ada1
Mmateus106 <rm98524@fiap.com.br>
2024-06-03 23:39:52 -0300

    Código atualizado. Rodando normalmente.

--- src/main/java/br/com/fiap/ferramenta/FerramentaApplication.java
+++ src/main/java/br/com/fiap/ferramenta/FerramentaApplication.java
@@ -3,7 +3,9 @@ package br.com.fiap.ferramenta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class FerramentaApplication {
 
